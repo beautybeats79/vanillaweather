d8b66ba8dd19f885645b36fe5425f67c41caaa80
beautybeats79 <beautybeats79@gmail.com>
2025-07-20 19:56:54 +0800

    Update weather key

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  const apiKey = "b2a5adcct04b33178913oc335f405433"; // SheCodes API key
+  const apiKey = "f0aaa5691cfa79897f985t035b4a46fo"; // SheCodes API key
   const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl)
     .then(refreshWeather)
